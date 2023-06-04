d9cdd1ba4f800634bf9637e72a7e119bcb12cd16
Laiamdelprado <lmarcodelprado@gmail.com>
2023-06-04 13:33:13 +0200

    core(): add translate

--- porfolio-lmdp/src/app/app.component.ts
+++ porfolio-lmdp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'porfolio-lmdp';
+
+  constructor(public translateService: TranslateService) {
+    this.translateService.addLangs(['en', 'es']);
+    this.translateService.use(environment.defaultLanguage);
+  }
+
 }
